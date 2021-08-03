84889f682a163d4131b71c2f9f573d425338a758
hkidd <harrisonakidd@gmail.com>
2021-08-02 19:04:16 -0500

    Edited login info

--- public/js/login.js
+++ public/js/login.js
@@ -23,14 +23,16 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
+    const firstname = document.querySelector('#firstname-signup').value.trim();
+    const lastname = document.querySelector('#lastname-signup').value.trim();
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && email && password) {
+    if (firstname && lastname && username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ firstname, lastname, username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
