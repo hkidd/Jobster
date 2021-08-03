3bb026655757b87fa22c80c3889fe2d38a8480ac
mguppy <mguppy34@gmail.com>
2021-08-02 21:00:29 -0500

    all models and seeds updated correctly

---
+++ models/Test.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Test extends Model {}
+
+Test.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        test_date: {
+            type: DataTypes.DATE,
+        },
+        concepts: {
+            type: DataTypes.TEXT,
+        },
+        passed: {
+            type: DataTypes.BOOLEAN,
+        },
+        application_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'application',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'test',
+    }
+);
+
+module.exports = Test;
