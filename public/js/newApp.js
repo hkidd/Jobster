a8559e1ff961e9626522cc137b6ebfca421e84ec
hkidd <harrisonakidd@gmail.com>
2021-08-04 12:37:57 -0500

    RFormatted js files

--- public/js/newApp.js
+++ public/js/newApp.js
@@ -1,63 +1,60 @@
 // We most likely need a front end js file that will run 3 routes (Application, Interview, and Test), after collecting the user input from the newApp handlebar page
 
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Using this template, need to grab the user input values from the application section of newApp
-    const first_name = document.querySelector('#firstname-signup').value.trim();
+  event.preventDefault();
 
-  
-    if (first_name && last_name) {
-      const response = await fetch('/api/users/application', {
-        method: 'POST',
-        body: JSON.stringify({ first_name, last_name }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to add new application.');
-      }
+  // Using this template, need to grab the user input values from the application section of newApp
+  const first_name = document.querySelector("#firstname-signup").value.trim();
+
+  if (first_name && last_name) {
+    const response = await fetch("/api/users/application", {
+      method: "POST",
+      body: JSON.stringify({ first_name, last_name }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add new application.");
     }
+  }
 
-    // Using this template, need to grab the user input values from the interview section of newApp
-    const first_name = document.querySelector('#firstname-signup').value.trim();
+  // Using this template, need to grab the user input values from the interview section of newApp
+  const first_name = document.querySelector("#firstname-signup").value.trim();
 
-  
-    if (first_name && last_name) {
-      const response = await fetch('/api/users/interview', {
-        method: 'POST',
-        body: JSON.stringify({ first_name, last_name }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to add new application.');
-      }
+  if (first_name && last_name) {
+    const response = await fetch("/api/users/interview", {
+      method: "POST",
+      body: JSON.stringify({ first_name, last_name }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add new application.");
     }
+  }
 
-    // Using this template, need to grab the user input values from the application section of newApp
-    const first_name = document.querySelector('#firstname-signup').value.trim();
+  // Using this template, need to grab the user input values from the application section of newApp
+  const first_name = document.querySelector("#firstname-signup").value.trim();
 
-  
-    if (first_name && last_name) {
-      const response = await fetch('/api/users/test', {
-        method: 'POST',
-        body: JSON.stringify({ first_name, last_name }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to add new application.');
-      }
+  if (first_name && last_name) {
+    const response = await fetch("/api/users/test", {
+      method: "POST",
+      body: JSON.stringify({ first_name, last_name }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add new application.");
     }
-  };
+  }
+};
 
-  document
-  .querySelector('.newapp-form')
-  .addEventListener('submit', signupFormHandler);
+document
+  .querySelector(".newapp-form")
+  .addEventListener("submit", signupFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,31 +1,31 @@
 // Here, this file is grabbing the user input from the sign up sheet
-  // This should then be sent to the back end at /api/users to POST a new user
-  // If successful, they are routed to the homepage
-  // If not, error is displayed and can try to sign up again
+// This should then be sent to the back end at /api/users to POST a new user
+// If successful, they are routed to the homepage
+// If not, error is displayed and can try to sign up again
 
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const first_name = document.querySelector('#firstname-signup').value.trim();
-    const last_name = document.querySelector('#lastname-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (first_name && last_name && email && password) {
-      const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({ first_name, last_name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to sign up.');
-      }
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const first_name = document.querySelector("#firstname-signup").value.trim();
+  const last_name = document.querySelector("#lastname-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (first_name && last_name && email && password) {
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      body: JSON.stringify({ first_name, last_name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up.");
     }
-  };
+  }
+};
 
-  document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
