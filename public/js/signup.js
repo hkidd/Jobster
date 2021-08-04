76094b522ed2afc2b620ac007d3c6217530df99e
hkidd <harrisonakidd@gmail.com>
2021-08-04 12:34:41 -0500

    Removed console logs

--- public/js/signup.js
+++ public/js/signup.js
@@ -10,11 +10,6 @@
     const last_name = document.querySelector('#lastname-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
-    console.log(first_name)
-    console.log(last_name);
-    console.log(email);
-    console.log(password);
   
     if (first_name && last_name && email && password) {
       const response = await fetch('/api/users/signup', {
