9e62a63d597637531367ea85eb9eaf0dc43582b9
mguppy <mguppy34@gmail.com>
2021-08-04 12:36:32 -0500

    working on the new app js file

--- public/js/newApp.js
+++ public/js/newApp.js
@@ -1,13 +1,16 @@
 // We most likely need a front end js file that will run 3 routes (Application, Interview, and Test), after collecting the user input from the newApp handlebar page
 
-const signupFormHandler = async (event) => {
+const newAppFormHandler = async (event) => {
     event.preventDefault();
   
     // Using this template, need to grab the user input values from the application section of newApp
-    const first_name = document.querySelector('#firstname-signup').value.trim();
+    const company_name = document.querySelector('#inputCompany').value.trim();
+    const job_url = document.querySelector('#jobURL').value.trim();
+    const date_found = document.querySelector('#dateFound').value.trim();
+    const role = document.querySelector('#role').value.trim();
 
   
-    if (first_name && last_name) {
+    if (company_name && job_url && date_found && role) {
       const response = await fetch('/api/users/signup', {
         method: 'POST',
         body: JSON.stringify({ first_name, last_name }),
