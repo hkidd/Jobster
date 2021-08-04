66821a6ea0ed860ab4433b9b138b1b63f3c9ac75
Christyn Garcia <christynsandusky@gmail.com>
2021-08-04 12:32:26 -0500

    working on userInfo

--- controllers/application-routes.js
+++ controllers/application-routes.js
@@ -9,7 +9,7 @@ const withAuth = require('../utils/auth');
 
 // GET all applications for homepage
 router.get('/', withAuth, async(req, res) => {
-    
+
     try {
         const dbApplicationData = await Application.findAll({
             include: [{
@@ -127,7 +127,7 @@ router.get('/signup', (req, res) => {
 
 // About us route
 router.get('/aboutus', (req, res) => {
-  res.render('aboutus');
+    res.render('aboutus');
 });
 
 // New job application route
@@ -138,7 +138,11 @@ router.get('/newApp', (req, res) => {
 router.get('/editApp', (req, res) => {
     res.render('editApp');
 });
+// User Info route
+router.get('/userInfo', (req, res) => {
+    res.render('userInfo');
+});
+
 
 
 module.exports = router;
-
