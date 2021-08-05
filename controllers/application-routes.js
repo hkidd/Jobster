2e1a14d60424f345a9e045b2276bd8d1ede81f7d
hkidd <harrisonakidd@gmail.com>
2021-08-04 21:06:52 -0500

    Reformatted app routes

--- controllers/application-routes.js
+++ controllers/application-routes.js
@@ -1,174 +1,173 @@
-const router = require('express').Router();
-const { Application, Interview, Test } = require('../models');
+const router = require("express").Router();
+const { Application, Interview, Test } = require("../models");
 // Import the custom middleware
-const withAuth = require('../utils/auth');
+const withAuth = require("../utils/auth");
 
 // Need GET, POST, PUT, and DELETE routes for the job applications.
 
 // These will be presented on the home page after user login using handlebars
 
-// Authentication function is running to check for logged in user 
+// Authentication function is running to check for logged in user
 
 // GET all applications for homepage
-router.get('/', withAuth, async(req, res) => {
-    
-    try {
-        const dbApplicationData = await Application.findAll({
-          where: {user_id: req.session.user},
-            include: [{
-                    model: Interview,
-                    attributes: ["id", "interview_number", "interview_date", "thank_you_note_sent", "follow_up_email", "application_id"],
-                },
-                {
-                    model: Test,
-                    attributes: ["id", "test_date", "concepts", "passed", "application_id"],
-                }
-            ],
-        }, 
-      );
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const dbApplicationData = await Application.findAll({
+      where: { user_id: req.session.user },
+      include: [
+        {
+          model: Interview,
+          attributes: [
+            "id",
+            "interview_number",
+            "interview_date",
+            "thank_you_note_sent",
+            "follow_up_email",
+            "application_id",
+          ],
+        },
+        {
+          model: Test,
+          attributes: [
+            "id",
+            "test_date",
+            "concepts",
+            "passed",
+            "application_id",
+          ],
+        },
+      ],
+    });
 
-        //   res.status(200).json(dbApplicationData);
-        // } catch (err) {
-        //   res.status(400).json(err);
-        // }
+    //   res.status(200).json(dbApplicationData);
+    // } catch (err) {
+    //   res.status(400).json(err);
+    // }
 
-        const applications = dbApplicationData.map((apps) =>
-            apps.get({ plain: true })
-        );
+    const applications = dbApplicationData.map((apps) =>
+      apps.get({ plain: true })
+    );
 
-        res.render('homepage', {
-            applications,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+    res.render("homepage", {
+      applications,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", withAuth, async(req, res) => {
-    // create a new application
-    console.log("post route")
-    try {
-        const applicationData = await Application.create({
-            company_name: req.body.company_name,
-            role: req.body.role,
-            job_url: req.body.job_url,
-            submission_date: req.body.submission_date,
-            date_found: req.body.date_found,
-            application_status: req.body.application_status,
-            user_id: req.session.user,
-        });
-        // if the application is successfully created, the new response will be returned as json
-        res.status(200).json(applicationData);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+router.post("/", withAuth, async (req, res) => {
+  // create a new application
+  console.log("post route");
+  try {
+    const applicationData = await Application.create({
+      company_name: req.body.company_name,
+      role: req.body.role,
+      job_url: req.body.job_url,
+      submission_date: req.body.submission_date,
+      date_found: req.body.date_found,
+      application_status: req.body.application_status,
+      user_id: req.session.user,
+    });
+    // if the application is successfully created, the new response will be returned as json
+    res.status(200).json(applicationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', withAuth, async(req, res) => {
+router.put("/:id", withAuth, async (req, res) => {
   // Update an application by id
-    try {
-        const applicationData = await Application.update({
-            company_name: req.body.company_name,
-            role: req.body.role,
-            job_url: req.body.job_url,
-            submission_date: req.body.submission_date,
-            date_found: req.body.date_found,
-            user_id: req.session.user,
-        }, {
-            where: {
-                id: req.params.id,
-            },
-        });
-        // if the application is successfully updated, the response will be returned as json
-        res.status(200).json(applicationData);
-    } catch (err) {
-        res.status(500).json(err);
-    };
+  try {
+    const applicationData = await Application.update(
+      {
+        company_name: req.body.company_name,
+        role: req.body.role,
+        job_url: req.body.job_url,
+        submission_date: req.body.submission_date,
+        date_found: req.body.date_found,
+        user_id: req.session.user,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    // if the application is successfully updated, the response will be returned as json
+    res.status(200).json(applicationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", withAuth, async(req, res) => {
-    // delete a application by its `id` value
-    try {
-        const applicationData = await Application.destroy({
-            where: {
-                id: req.params.id,
-            },
-        });
+router.delete("/:id", withAuth, async (req, res) => {
+  // delete a application by its `id` value
+  try {
+    const applicationData = await Application.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-        if (!applicationData) {
-            res.status(404).json({ message: "No application found with this id!" });
-            return;
-        }
-
-        res.status(200).json(applicationData);
-    } catch (err) {
-        res.status(500).json(err);
+    if (!applicationData) {
+      res.status(404).json({ message: "No application found with this id!" });
+      return;
     }
+
+    res.status(200).json(applicationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Login route
-router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect to the homepage
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    // Otherwise, render the 'login' template
-    res.render('login');
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render("login");
 });
 
 // Signup route
-router.get('/signup', (req, res) => {
-    // If the user is already logged in, redirect to the homepage
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    // Otherwise, render the 'signup' template
-    res.render('signup');
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  // Otherwise, render the 'signup' template
+  res.render("signup");
 });
-
 
 // About us route
-router.get('/aboutus', (req, res) => {
-    res.render('aboutus', {
-      loggedIn: req.session.loggedIn
-    });
+router.get("/aboutus", (req, res) => {
+  res.render("aboutus", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 // New job application route
-router.get('/newApp', (req, res) => {
-    // create a new application
-    // try {
-    //     const applicationData = await Application.create({
-    //         company_name: req.body.company_name,
-    //         role: req.body.role,
-    //         job_url: req.body.job_url,
-    //         submission_date: req.body.submission_date,
-    //         date_found: req.body.date_found,
-    //         user_id: req.body.user_id,
-    //     });
-    //     // if the application is successfully created, the new response will be returned as json
-    //     res.status(200).json(applicationData);
-    // } catch (err) {
-    //     res.status(400).json(err);
-    // }
-    res.render('newApp', {
-      loggedIn: req.session.loggedIn
-    });
+router.get("/newApp", (req, res) => {
+  res.render("newApp", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 // Edit application route
-router.get('/editApp', (req, res) => {
-    res.render('editApp', {
-      loggedIn: req.session.loggedIn
-    });
+router.get("/editApp", (req, res) => {
+  res.render("editApp", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 // User Info route
-router.get('/userInfo', (req, res) => {
-    res.render('userInfo', {
-      loggedIn: req.session.loggedIn
-    });
+router.get("/userInfo", (req, res) => {
+  res.render("userInfo", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
 module.exports = router;
