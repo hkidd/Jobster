3a16d82c732dadfd98353d30a7b2003da07504ab
Carlos Perez <ca.perez.barr@gmail.com>
2021-08-05 21:21:16 -0500

    Update: Added multer functionality

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+const fs = require('fs');
+const router = require('express').Router()
 
 //Multer Imports
 const multer = require('multer');
@@ -19,6 +21,9 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+//Methods Imports
+const User = require('./models/User');
 
 //App Initialization
 const app = express();
@@ -38,13 +43,82 @@ app.use(session(sess));
 const hbs = exphbs.create({ helpers });
 
 
-app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'handlebars');
-app.engine('handlebars', hbs.engine);
+app.use(routes);
+
+
+//Multer Storage Specifications
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads")
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname)
+  }
+});
+
+
+
+//Multer Filter
+const upload = multer({
+  storage: fileStorageEngine,
+  fileFilter: function (req, file, cb) {
+    //Allowed ext
+    const filetypes = /jpg|jpeg|png|gif/
+    // const filetypes = /pdf/
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (extname) {
+      return cb(null, true);
+    } else {
+      cb('Error: Invalid file extension!')
+    }
+  }
+});
+
+
+
+//Post Image
+app.post('/single', upload.single('image'), (req, res) => {
+  // console.log(req.file);
+  const savedImage = req.file;
+  const imageLocation = savedImage.path;
+  // console.log(savedImage);
+
+  cloudinary.uploader.upload(imageLocation, { tags: "avatar" }).then(data => {
+    // console.dir(data);
+    // console.log(data);
+
+    if (data.url) {
+      User.update({
+        image_url: imageLocation
+      },
+      {
+        where: {
+          first_name: 'John'
+        }
+      })
+        .then((newUserProfilePic) => {
+          res.json(newUserProfilePic);
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    }
+    fs.unlink(imageLocation, (error) => error ? console.log(error) : null)
+  });
+
+
+  // res.send(`${savedImage} Single File Upload Success`);
+});
+
 
 
 
